f9979220f853d406474d558485060854185b3def
David <d_men2a@yahoo.com>
2016-10-23 17:48:42 -0700

    Update phone.js

--- phone.js
+++ phone.js
@@ -1,55 +1,52 @@
 var PHONE_PRICE = 199.99;
-var TAX_RATE = 0.05;
 var ACCESSORY_PRICE = 19.99;
-var SPENDING_THRESHOLD = 900;
-var bankBalance = 1000;
-var totalPhonesPurchased = 0;
+var TAX_RATE = 0.09;
+var SPENDING_THRESHOLD = 100.00;
+var totalPairsBought = 0;
 
-function phoneCount() {
+function privateBankInfo() {
     "use strict";
-    totalPhonesPurchased += 1;
-}
-
-function subTotal(phonesTotal) {
-    "use strict";
-    return phonesTotal * (PHONE_PRICE + ACCESSORY_PRICE);
-}
-
-function addTax() {
-    "use strict";
-    return subTotal(totalPhonesPurchased) * TAX_RATE;
-}
-
-function totalPrice() {
-    "use strict";
-    return subTotal(totalPhonesPurchased) + addTax();
-}
-
-var total = totalPrice();
-
-while (total + PHONE_PRICE + ACCESSORY_PRICE < SPENDING_THRESHOLD) {
-    phoneCount();
-    console.log("Running phone total: " + totalPhonesPurchased);
+    var bankBalance = 1000;
     
-    total = totalPrice();
-    console.log("Running price total: " + total);
+    function accessBalance(balance) {
+        return balance;
+    }
+    
+    return accessBalance(bankBalance);
 }
+
+function subTotal(pairsBought) {
+    "use strict";
+    return pairsBought * (PHONE_PRICE + ACCESSORY_PRICE);
+}
+
+function calcTax(pairsTaxed) {
+    "use strict";
+    return subTotal(pairsTaxed) * TAX_RATE;
+}
+
+function totalPrice(pairsBought) {
+    "use strict";
+    return subTotal(pairsBought) + calcTax(pairsBought);
+}
+
+var total = totalPrice(totalPairsBought);
+
+while (privateBankInfo() - (total + PHONE_PRICE + ACCESSORY_PRICE) > SPENDING_THRESHOLD) {
+    totalPairsBought += 1;
+    console.log("Phone/accessory pairs bought so far: ", totalPairsBought);
+    
+    total = totalPrice(totalPairsBought);
+    console.log("Running total: ", total, "\n");
+}
+
+var moneyLeft = privateBankInfo() - total;
 
 function formatPrice(finalCost) {
     "use strict";
-    return finalCost.toFixed(2);
+    return "$" + finalCost.toFixed(2);
 }
 
-console.log("\nTotal phones purchased: " + totalPhonesPurchased);
-console.log("Total cost: $" + formatPrice(total));
-
-function checkBalance() {
-    "use strict";
-    if (total < bankBalance) {
-        return "\nYes, you can afford this.";
-    } else {
-        return "\nNo, you can't afford this.";
-    }
-}
-
-console.log("\nCan I afford this? " + checkBalance());
+console.log("Total phones/accessories pairs purchased: ", totalPairsBought);
+console.log("Grand total: ", formatPrice(total));
+console.log("Money left over: ", formatPrice(moneyLeft));
